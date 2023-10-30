e613bd83c469a39436f89706535c2c6dfe43b89d
Matheus904-17 <67837282+Matheus904-12@users.noreply.github.com>
2023-10-29 22:25:44 -0300

    Até amanhã

---
+++ .history/main_20231029222307.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('nav');
+
+    hamburgerMenu.addEventListener('click', function() {
+      nav.classList.toggle('active');
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const moedasDiv = document.querySelector('.moedas');
+    moedasDiv.classList.add('loaded');
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const overlayTexts = document.querySelectorAll('.overlay-text h3');
+
+    function animateText(index) {
+      if (index < overlayTexts.length) {
+        setTimeout(function() {
+          overlayTexts[index].style.opacity = 1;
+          overlayTexts[index].style.transform = 'translateY(0)';
+          animateText(index + 1);
+        }, 200);
+      }
+    }
+
+    animateText(0);
+  });
